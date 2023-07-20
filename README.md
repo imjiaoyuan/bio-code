cd41cd94d9d3a90409ab342fbdff9ae46ac7a543
hieroglyphs <hieroglyphs@foxmail.com>
2023-07-20 13:33:54 +0800

    example

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is an example of the Cystatin family in Oryza_Sativa.
---
+++ extract_sequence.py
@@ -0,0 +1,13 @@
+from Bio import SeqIO
+
+def extract_sequences(id_list, fasta_file, out_file):
+    with open(id_list, 'r') as id_handle, open(out_file, 'w') as out_handle:
+        ids = [line.strip() for line in id_handle.readlines()]
+        fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, str(fasta.seq)
+            if name in ids:
+                SeqIO.write(fasta, out_handle, 'fasta')
+
+extract_sequences('id_list.txt', './protein_sequence/SPECIES.fasta', 'protein_for_target_id.fasta')
