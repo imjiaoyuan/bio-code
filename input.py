b6f38bb14e8f6cc99b4db8e61bf7867f9ae4a8f7
Hieroglyphs <hieroglyphs@foxmail.com>
2023-09-24 10:06:39 +0800

    update

--- input.py
+++ input.py
@@ -2,6 +2,6 @@ PF_number = "PF00403"
 
 evaluation_threshold = "1e-5"
 
-species = "Lolium_perenne"
+species = "Oryza_sativa"
 
 # The repository only contains the genome protein sequences of three species: Arabidopsis_thaliana , Oryza_sativa , and Zea_mays
