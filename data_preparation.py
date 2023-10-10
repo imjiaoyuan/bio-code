d14a478cb841385b5c3f70d9aa6f03e76c27974f
hieroglyphs <hieroglyphs@foxmail.com>
2023-10-10 13:10:41 +0800

    update database

--- data_preparation.py
+++ data_preparation.py
@@ -1,26 +1,26 @@
 import requests
 import input
 
-hmm = "http://pfam-legacy.xfam.org/family/{}/hmm".format(input.PF_number)  # 文件的URL地址
+hmm = "https://www.ebi.ac.uk/interpro/wwwapi//entry/pfam/{}?annotation=hmm".format(input.PF_number)
 
-hmm_content = requests.get(hmm)  # Send a GET request to obtain file content
+hmm_content = requests.get(hmm)
 
-hmm_content = hmm_content.text  # Obtain the text content of the file
+hmm_content = hmm_content.text
 
 hmm_file = open("build.hmm", "w")
 
-hmm_file.write(hmm_content)  # Write the obtained content to a file
+hmm_file.write(hmm_content)
 
 hmm_file.close()
 
-PFseed = "http://pfam-legacy.xfam.org/family/{}/alignment/seed/format?format=stockholm&alnType=seed&order=t&case=l&gaps=default&download=1".format(input.PF_number)
+PFseed = "https://www.ebi.ac.uk/interpro/wwwapi//entry/pfam/{}/?annotation=alignment:seed&download".format(input.PF_number)
 
-PFseed_content = requests.get(PFseed)  # Send a GET request to obtain file content
+PFseed_content = requests.get(PFseed)
 
-PFseed_content = PFseed_content.text  # Obtain the text content of the file
+PFseed_content = PFseed_content.text
 
 PFseed_file = open("PFseed.txt", "w")
 
-PFseed_file.write(PFseed_content)  # Write the obtained content to a file
+PFseed_file.write(PFseed_content)
 
 PFseed_file.close()
