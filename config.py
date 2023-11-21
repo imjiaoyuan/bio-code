448f0edc801b09af6f8d06dbbabfeda9dbb79992
unclecat4 <hieroglyphs@foxmail.com>
2023-11-21 10:12:27 +0800

    new pipeline

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ PF_number = "PF00403"
 
 evaluation_threshold = "1e-5"
 
-species = "Arabidopsis_thaliana"
+species = "Oryza_sativa"
 
 # The repository only contains the genome protein sequences of three species: Arabidopsis_thaliana , Oryza_sativa , Lolium_perenne , and Zea_mays
